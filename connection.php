664badadc44ecd80293590fec47a6d0943290266
devfodex <62177135+devfodex@users.noreply.github.com>
2021-03-28 12:38:43 -0300

    primeiro commit

---
+++ connection.php
@@ -0,0 +1,30 @@
+<?php
+
+class MySql
+{
+    private static $pdo;
+    public static function connect()
+    {
+        if (self::$pdo == null)
+        {
+            try
+            {
+                self::$pdo = new PDO('mysql:host=' . HOST . ';dbname=' . DATABASE, USER, PASSWORD, array(
+                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                ));
+
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+            }
+            catch(Exception $e)
+            {
+                echo 'Erro ao estabelecer conexão com o banco de dados';
+            }
+        }
+        return self::$pdo;
+    }
+    
+    //===================================
+}
+
+?>
